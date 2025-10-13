70d96407b8cb64606be9533c978efbd5d381d54f
awa Liny <163953777+awaLiny2333@users.noreply.github.com>
2025-10-13 14:20:02 +0800

    improve: Now shows the build time in AppSettings! other: latest build!

--- home/hvigorfile.ts
+++ home/hvigorfile.ts
@@ -1,6 +1,19 @@
 import { hapTasks } from '@ohos/hvigor-ohos-plugin';
 
 export default {
-    system: hapTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
-    plugins:[]         /* Custom plugin to extend the functionality of Hvigor. */
+  system: hapTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
+  plugins: [], /* Custom plugin to extend the functionality of Hvigor. */
+  config: {
+    ohos: {
+      overrides: {
+        buildOption: {
+          arkOptions: {
+            buildProfileFields: {
+              linysBuildTime: (new Date().toUTCString()),
+            }
+          }
+        }
+      }
+    }
+  }
 }
